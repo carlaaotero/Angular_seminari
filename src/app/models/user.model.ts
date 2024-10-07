081e9d0e5e66a85d4e71202c22ba7cd28565f588
danielperezflorit <daniel.perez.florit@estudiantat.upc,edu>
2024-10-07 20:18:26 +0200

    Funciona Usuaris

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,8 +1,8 @@
 export interface User {
-    id: string;
-    name: string;
-    mail: string;
-    password: string;
-    comment: string;
-  }
+  _id?: string;      // MongoDB genera automáticamente este campo al insertar
+  name: string;
+  mail: string;     // Añadir el campo email
+  password: string;
+  comment: string;    // Este campo es la "Biografía"
+}
   
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,33 +2,35 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
+//import { environment } from '../environments/environment';  // Importar environment
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:3000/users';
+  private apiUrl = "http://localhost:3000/api/user";  // Usar apiUrl desde environment
 
   constructor(private http: HttpClient) {}
 
+  // Obtener todos los usuarios
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.apiUrl);
   }
 
-  getUserById(id: string): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/${id}`);
+  // Agregar un nuevo usuario
+  addUser(usuario: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, usuario);
   }
 
-  createUser(user: User): Observable<User> {
-    return this.http.post<User>(this.apiUrl, user);
+  // Actualizar un usuario existente
+  updateUser(usuario: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/${usuario._id}`, usuario);
   }
 
-  updateUser(id: string, user: User): Observable<User> {
-    return this.http.put<User>(`${this.apiUrl}/${id}`, user);
-  }
-
-  deleteUser(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  // Eliminar un usuario por su _id
+  deleteUserById(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
   }
 }
+
 
